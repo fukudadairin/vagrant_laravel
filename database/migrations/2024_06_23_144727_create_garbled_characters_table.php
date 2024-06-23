0062a9f9570fbf31085d2232473e9aa89ba45fb4
fukudadairin <79754701+fukudadairin@users.noreply.github.com>
2024-06-23 15:05:49 +0000

    Handing Garbled caracter

---
+++ database/migrations/2024_06_23_144727_create_garbled_characters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('garbled_characters', function (Blueprint $table) {
+            $table->id();
+	    $table->timestamps();
+
+	    // 文字エンコーディングと照合順序の設定
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('garbled_characters');
+    }
+};
